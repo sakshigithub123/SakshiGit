3318d20c71d931c1e0ee82efc285ca42321f97cf
sakshi <sakshidhakite20@gmail.com>
2023-07-27 14:38:14 -0700

    First Code

---
+++ src/com/demo/git/Demo1.java
@@ -0,0 +1,13 @@
+package com.demo.git;
+import java.util.ArrayList;
+public class Demo1 
+{
+	public static void main(String [] args)
+	{
+		ArrayList<Integer> al = new ArrayList<>();
+		al.add(10);
+		al.add(20);
+		al.add(30);
+		System.out.println(al);
+	}
+}
